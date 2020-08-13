da0b5d79c221bc4a4148ccf412340801cbca651d
AndraVasilache <vasilacheandragabriela@gmail.com>
2020-08-14 01:01:17 +0300

    start adding the menu item

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import type PluginManager from '@gmod/jbrowse-core/PluginManager'
 import Plugin from '@gmod/jbrowse-core/Plugin'
+import StorageIcon from '@material-ui/icons/Storage'
+import { getSession, isSessionModelWithWidgets } from '@gmod/jbrowse-core/util'
 
 import EnsemblVEPWidget from './EnsemblVEPWidget'
+import { autorun } from 'mobx'
 export default class extends Plugin {
-  name = 'GDCPlugin'
+  name = 'EnsemblPlugin'
 
   install(pluginManager: PluginManager) {
     const AdapterType =
@@ -27,6 +30,50 @@ export default class extends Plugin {
         configSchema,
         stateModel,
         ReactComponent,
+      })
+    })
+  }
+
+  configure(pluginManager: PluginManager) {
+    const session = pluginManager.rootModel?.session
+    const alreadyAdded: string[] = []
+    autorun(() => {
+      const { views } = session || { views: [] }
+      views.forEach(view => {
+        if (view.type === 'LinearGenomeView') {
+          const { tracks } = view
+          tracks.forEach(track => {
+            if (
+              track.type === 'VariantTrack' &&
+              !alreadyAdded.includes(track.id)
+            ) {
+              alreadyAdded.push(track.id)
+              track.addAdditionalContextMenuItemCallback(
+                (feature, track, pluginManager) => {
+                  const { session } = pluginManager.rootModel
+                  const menuItem = {
+                    /* menu item you already have, it has "label", "icon", "onClick" */
+                    label: 'Ensembl VEP',
+                    icon: StorageIcon,
+                    onClick: () => {
+                      console.log('hello')
+                      const session = getSession(self)
+                      if (isSessionModelWithWidgets(session)) {
+                        const featureWidget = session.addDrawerWidget(
+                          'EnsemblVEPDrawerWidget',
+                          'ensemblVEPDrawerWidget',
+                          { featureData: self.contextMenuFeature?.toJSON() },
+                        )
+                        session.showDrawerWidget(featureWidget)
+                      }
+                  }
+                }
+                  return [menuItem]
+                },
+              )
+            }
+          })
+        }
       })
     })
   }
